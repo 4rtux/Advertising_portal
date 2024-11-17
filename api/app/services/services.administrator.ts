a4c0287ea99db54c8861207ecba227884fa784cb
Okpaleke Chukwudi <okpaleke34@gmail.com>
2024-11-17 15:08:49 +0100

    feat: Updated services

--- api/app/services/services.administrator.ts
+++ api/app/services/services.administrator.ts
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import {IUserRepository, UserRepository,IUserOptional, IUser} from "../infrastructure/sqlite/repository/user.repository";
+import {IAdministratorRepository, AdministratorRepository,IAdministratorOptional, IAdministrator} from "../infrastructure/sqlite/repository/administrator.repository";
 import { JWT_SECRET, SMTP_SETTINGS_1, createToken, sendMail, updateObject } from "../utils/helpers";
 import moment from 'moment';
 import fs from 'fs';
@@ -13,47 +13,42 @@ export interface IMessge{
     data?:any;
     message?:string;
 }
-export default class UserService {
-    private repo: IUserRepository;
+export default class AdministratorService {
+    private repo: IAdministratorRepository;
 
     constructor(){
-        this.repo = new UserRepository();
+        this.repo = new AdministratorRepository();
     }
 
     
-    async createUser(user: IUserOptional): Promise<IMessge>{
+    async createAdministrator(administrator: IAdministratorOptional): Promise<IMessge>{
         try{
-            //Verify if username exists
-            let users = await this.repo.findByVal({username:user.username})
-            if(users.length > 0){
-                return {'status':false,data:{'message':'Username already exists'}}
-            }
             // Verify if email already exists
-            users = await this.repo.findByVal({email:user.email})
-            if(users.length > 0){
+            let administrators = await this.repo.findByVal({email:administrator.email})
+            if(administrators.length > 0){
                 return {'status':false,data:{'message':'Email already exists'}}
             }
             // Verify if phone already exists
-            users = await this.repo.findByVal({phone:user.phone})
-            if(users.length > 0){
+            administrators = await this.repo.findByVal({phone:administrator.phone})
+            if(administrators.length > 0){
                 return {'status':false,data:{'message':'Phone number already exists'}}
             }
 
 
             const id = new Identity()
-            user.password = crypto.createHash('md5').update(user.password+"").digest('hex');
+            administrator.password = crypto.createHash('md5').update(administrator.password+"").digest('hex');
             const timestamp = Math.floor(moment().valueOf()/1000)
-            user.status = 1
-            user.last_seen = new Date()
+            administrator.status = 1
+            administrator.last_seen = new Date()
             
-            const createdUser = await this.repo.create(user)
-            if(createdUser){
-                const token = createToken("auth",{userID: createdUser.id, role: "user"})
+            const createdAdministrator = await this.repo.create(administrator)
+            if(createdAdministrator){
+                const token = createToken("auth",{administratorID: createdAdministrator.id, role: "administrator"})
                 const dayToken = createToken("dayToken",{timestamp, token: id.generateID(5,"alpha")+timestamp})
-                return {'status':true,data:{'message':'User created successfully','user':user,'token':token,dayToken}}             
+                return {'status':true,data:{'message':'Administrator created successfully','administrator':administrator,'token':token,dayToken}}             
             }
             else{
-                return {'status':false,data:{'message':'User creation failed'}}
+                return {'status':false,data:{'message':'Administrator creation failed'}}
             }
         }
         catch(err:any){
@@ -61,29 +56,26 @@ export default class UserService {
         }
     }
 
-    async resetPassword(userIdentifier: string): Promise<IMessge>{
+    async resetPassword(administratorIdentifier: string): Promise<IMessge>{
         try{
-            let users = await this.repo.findByVal({username:userIdentifier})
-            if(users.length < 1){
-                users = await this.repo.findByVal({email:userIdentifier})
+            const administrators = await this.repo.findByVal({email:administratorIdentifier})
+            if(administrators.length !== 1){
+                return {'status':false,data:{'message':'Administrator not found'}}
             }
-            if(users.length !== 1){
-                return {'status':false,data:{'message':'User not found'}}
-            }
-            const user = users[0]
+            const administrator = administrators[0]
             const id = new Identity()
             // const timestamp = Math.floor(moment().valueOf()/1000)
             const password = id.generateID(10,"mix")
             const hashedPassword = crypto.createHash('md5').update(password).digest('hex');
 
-            const update  = await this.repo.update({password:hashedPassword},{id:user.id})
+            const update  = await this.repo.update({password:hashedPassword},{id:administrator.id})
             if(update){
-                const message = `Dear ${user.first_name}, <br/><br/> Your password has been reset to ${password}.<br/>Thanks`
+                const message = `Dear ${administrator.first_name}, <br/><br/> Your password has been reset to ${password}.<br/>Thanks`
                 let contents = fs.readFileSync('./app/data/mail.html').toString()
                 // let contents = fs.readFileSync('../data/mail.html').toString()
                 contents = contents.replace(/==mail-title==/gi,"Password Reset")
                 const html = contents.replace(/==mail-body==/gi,message)
-                sendMail({settings:SMTP_SETTINGS_1,subject:`SignedIn: Password Reset`,html,to:user.email,callback:function(error, info){
+                sendMail({settings:SMTP_SETTINGS_1,subject:`SignedIn: Password Reset`,html,to:administrator.email,callback:function(error, info){
                     if (error) {
                         console.log({error});
                         return {'status':false,data:{'message':'Password reset successful but mail not sent'}}
@@ -102,24 +94,21 @@ export default class UserService {
 
 
 
-    async userLogin(Identifier: string,password:string): Promise<IMessge>{
+    async administratorLogin(Identifier: string,password:string): Promise<IMessge>{
         try{
             password = crypto.createHash('md5').update(password).digest('hex');
-            let users = await this.repo.findByVal({username:Identifier, password})
-            if(users.length < 1){
-                users = await this.repo.findByVal({email:Identifier, password})
-            }
-            if(users.length < 1){
-                users = await this.repo.findByVal({phone:Identifier, password})
+            let administrators = await this.repo.findByVal({email:Identifier, password})
+            if(administrators.length < 1){
+                administrators = await this.repo.findByVal({phone:Identifier, password})
             }
 
-            if(users.length == 1){
-                const user =  users[0]
+            if(administrators.length == 1){
+                const administrator =  administrators[0]
                 const timestamp = Math.floor(moment().valueOf()/1000)
                 const id = new Identity()
-                const token = createToken("auth",{userID: user.id, role: "user"})
+                const token = createToken("auth",{administratorID: administrator.id, role: "administrator"})
                 const dayToken = createToken("dayToken",{timestamp, token: id.generateID(5,"alpha")+timestamp})
-                return {'status':true,data:{'message':'User signed in successfully','user':user,'token':token,dayToken}}
+                return {'status':true,data:{'message':'Administrator signed in successfully','administrator':administrator,'token':token,dayToken}}
             }
             else{  
                 return {'status':false,data:{'message':'Invalid login details'}}
@@ -131,17 +120,17 @@ export default class UserService {
     }
     
 
-    async createUserToken(users:IUserOptional[],userAgent:any): Promise<IMessge>{
+    async createAdministratorToken(administrators:IAdministratorOptional[],administratorAgent:any): Promise<IMessge>{
         try{
-            if(users.length == 0){
+            if(administrators.length == 0){
                 return {'status':false,data:{'message':'Invalid login details'}}
             }
-            else if(users.length == 1){
-                const user =  users[0]
-                return {'status':true,'data':{token:"token","privileges": "sortPrivileges(user.privileges,token,true)"}}
+            else if(administrators.length == 1){
+                const administrator =  administrators[0]
+                return {'status':true,'data':{token:"token","privileges": "sortPrivileges(administrator.privileges,token,true)"}}
             }
             else{
-                throw new Error("User exists more than once")
+                throw new Error("Administrator exists more than once")
             }
         }
         catch(err:any){
@@ -157,9 +146,9 @@ export default class UserService {
             confirmPassword = crypto.createHash('md5').update(confirmPassword).digest('hex');
             oldPassword = crypto.createHash('md5').update(oldPassword).digest('hex');
             if(password === confirmPassword){
-                const user = await this.repo.findByVal({id, password:oldPassword})
-                if(user.length === 1){
-                    const staff = user[0]
+                const administrator = await this.repo.findByVal({id, password:oldPassword})
+                if(administrator.length === 1){
+                    const staff = administrator[0]
                     const update  = await this.repo.update({password},{id})
                     if(update){
                         return {'status':true,data:{'message':'Password changed successfully'}}
@@ -181,31 +170,26 @@ export default class UserService {
         }
     }
     
-    async updateProfile(id:number,update:IUserOptional): Promise<IMessge>{
+    async updateProfile(id:number,update:IAdministratorOptional): Promise<IMessge>{
         try{
             //Verify if email exists
-            let user = await this.repo.findByVal({email:update.email})
-            if(user.length > 0 && user[0].id != id){
+            let administrator = await this.repo.findByVal({email:update.email})
+            if(administrator.length > 0 && administrator[0].id != id){
                 return {'status':false,data:{'message':'Email already exists'}}
-            }
-            //Verify if email exists
-            user = await this.repo.findByVal({username:update.username})
-            if(user.length > 0 && user[0].id != id){
-                return {'status':false,data:{'message':'Username already exists'}}
             }
             // Check if phone exists
             if(update.phone){
-                user = await this.repo.findByVal({phone:update.phone})
-                if(user.length > 0 && user[0].id != id){
+                administrator = await this.repo.findByVal({phone:update.phone})
+                if(administrator.length > 0 && administrator[0].id != id){
                     return {'status':false,data:{'message':'Phone already exists'}}
                 }
             }
-            const userDetail = await this.userDetails({id})
-            const updatedUser = updateObject(userDetail,update)
-            delete updatedUser.data
-            delete updatedUser.third_party
-            const updateUser = await this.repo.update(updatedUser,{id})
-            if(updateUser){
+            const administratorDetail = await this.administratorDetails({id})
+            const updatedAdministrator = updateObject(administratorDetail,update)
+            delete updatedAdministrator.data
+            delete updatedAdministrator.third_party
+            const updateAdministrator = await this.repo.update(updatedAdministrator,{id})
+            if(updateAdministrator){
                 return {'status':true,data:{'message':'Profile updated successfully'}}
             }
             else{
@@ -217,14 +201,14 @@ export default class UserService {
         }
     }
 
-    async userDetails(val:IUserOptional): Promise<IUser>{
+    async administratorDetails(val:IAdministratorOptional): Promise<IAdministrator>{
         try{
-            const users = await this.repo.findByVal(val)
-            if (users.length == 1) {
-                return users[0]
+            const administrators = await this.repo.findByVal(val)
+            if (administrators.length == 1) {
+                return administrators[0]
             }
             else{
-                throw new Error("User not found or user exists more than once")
+                throw new Error("Administrator not found or administrator exists more than once")
             }
         }
         catch(err:any){
